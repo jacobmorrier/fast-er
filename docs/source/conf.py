ff75f55794bd79f0e8148f629b093b6cfd18e507
Jacob Morrier <77862675+jacobmorrier@users.noreply.github.com>
2024-10-09 19:47:39 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+# /!\ need to call: pip install sphinx-book-theme
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
